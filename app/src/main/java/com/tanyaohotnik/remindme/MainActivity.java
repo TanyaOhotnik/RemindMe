46da645805c6959c893af992a209f67b7a0fdda8
TanyaOhotnik <tanyaohotnik3003@gmail.com>
2017-06-02 09:19:07 +0300

    Added menu and DrawerLayout Structure

--- app/src/main/java/com/tanyaohotnik/remindme/MainActivity.java
+++ app/src/main/java/com/tanyaohotnik/remindme/MainActivity.java
@@ -1,13 +1,51 @@
 package com.tanyaohotnik.remindme;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+        mToolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.search_menu:; return true;
+    }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
     }
 }
